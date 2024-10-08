84ccacbdfd1843eab22ab44c260ffc7304282ce3
lekxment <127068979+lekxment@users.noreply.github.com>
2024-10-09 00:20:36 +0800

    Add files via upload

--- week10/visualisations.js
+++ week10/visualisations.js
@@ -13,3 +13,13 @@ var vg_3 = "electricity_map.vg.json";
 vegaEmbed("#choropleth_map", vg_3).then(function(result) {
   // Access the Vega view instance as result.view
 }).catch(console.error);
+
+var vg_4 = "annual_freshwater_continents.vg.json";
+vegaEmbed("#donut_chart", vg_4).then(function(result) {
+  // Access the Vega view instance as result.view
+}).catch(console.error);
+
+var vg_5 = "gdp_and_population.vg.json";
+vegaEmbed("#scatter_plot", vg_5).then(function(result) {
+  // Access the Vega view instance as result.view
+}).catch(console.error);
